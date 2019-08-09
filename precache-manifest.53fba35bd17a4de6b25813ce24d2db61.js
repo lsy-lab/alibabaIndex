005017c8e63cd922b9ab82e753344a19d90ef381
lsy-lab <3099179710@qq.com>
2019-08-09 13:56:25 +0800

    Updates

---
+++ precache-manifest.53fba35bd17a4de6b25813ce24d2db61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a0903a5d302668110e925d80e0d4d1",
+    "url": "/aliIndex/index.html"
+  },
+  {
+    "revision": "b28630f1c9f00c897fe8",
+    "url": "/aliIndex/static/css/main.5db8e707.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/aliIndex/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "b28630f1c9f00c897fe8",
+    "url": "/aliIndex/static/js/main.45471d07.chunk.js"
+  },
+  {
+    "revision": "f93e671a971bbc6c64af",
+    "url": "/aliIndex/static/js/runtime~main.41da8910.js"
+  }
+]);
